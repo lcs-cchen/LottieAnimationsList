15770a6b18fa14b1166e7a6aca910088d7beabda
cyrus chen <pschen@lcs.on.ca>
2023-01-30 21:48:30 -0500

    Initial Commit

---
+++ LottieAnimationsList/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LottieAnimationsList
+//
+//  Created by Cyrus Chen on 30/1/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Cyrus Chen on 30/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
